488fe723b25cca61b6bce968a85c2684db57e42a
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2025-01-11 13:23:13 +0600

    Update assetPrefix and basePath in next.config.js for deployment path adjustment

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? "/portfolio/" : "",
-  basePath: isProd ? "/portfolio" : "",
+  assetPrefix: isProd ? "dipto1971/portfolio/" : "",
+  basePath: isProd ? "dipto1971/portfolio" : "",
   output: "export",
 };
 
