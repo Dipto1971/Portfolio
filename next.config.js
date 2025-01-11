45e5ae6c025554f0ffae42903aadf30ee6f9f465
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2025-01-11 13:46:56 +0600

    Update deployment configuration: change branch to gh-pages, simplify next.config.js, and remove unused scripts from package.json

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? "/dipto1971/portfolio/" : "",
-  basePath: isProd ? "/dipto1971/portfolio/" : "",
-  output: "export",
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 module.exports = nextConfig;
